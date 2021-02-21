00b397cbc8e9c585d258eaad741a349313ab672e
Deepesh Rathi <deepeshrathi5050@gmail.com>
2021-02-21 19:28:39 +0530

    login and register started

---
+++ Quizzer-App/app/src/main/java/com/example/quizzer_app/LoginActivity.java
@@ -0,0 +1,4 @@
+package com.example.quizzer_app;
+
+public class LoginActivity {
+}
--- Quizzer-App/app/src/main/java/com/example/quizzer_app/MainActivity.java
+++ Quizzer-App/app/src/main/java/com/example/quizzer_app/MainActivity.java
@@ -2,13 +2,36 @@ package com.example.quizzer_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button login;
+    private Button register;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        login = findViewById(R.id.login_button);
+        register = findViewById(R.id.register_button);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,RegisterActivity.class));
+                finish();
+            }
+        });
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,LoginActivity.class));
+                finish();
+            }
+        });
+
     }
 }
